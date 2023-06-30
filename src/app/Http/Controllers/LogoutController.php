06cce5a8dbde43a37d998fe43af6a0118f1a42f8
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2023-06-30 19:03:10 +0900

    Logout

---
+++ src/app/Http/Controllers/LogoutController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Aws\CognitoIdentityProvider\AdminUserGlobalSignOut\AdminUserGlobalSignOut;
+use App\Aws\CognitoIdentityProvider\AdminUserGlobalSignOut\AdminUserGlobalSignOutPayload;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+final readonly class LogoutController
+{
+    public function __construct(private AdminUserGlobalSignOut $adminUserGlobalSignOut)
+    {
+    }
+
+    public function __invoke(): RedirectResponse
+    {
+        if (Auth::check()) {
+            /** @var User $user */
+            $user = Auth::user();
+
+            $payload = AdminUserGlobalSignOutPayload::create($user->username);
+            $this->adminUserGlobalSignOut->execute($payload);
+
+            Auth::logout();
+            Session::invalidate();
+            Session::regenerateToken();
+        }
+
+        return redirect()->route('welcome');
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,4 +28,5 @@ Route::post('/login', LoginController::class)->name('login');
 
 Route::group(['middleware' => 'auth'], static function () {
     Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', LogoutController::class)->name('logout');
 });
